708915c76d3dab83bd0c40c0104365482c239329
Ricky Tang <tangricky001@gmail.com>
2025-12-19 20:29:57 -0500

    feat: initialize ios xcode project

---
+++ ios/SleepTracker/SleepTracker/SleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackerApp.swift
+//  SleepTracker
+//
+//  Created by Ricky Tang on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
