ca36a07bc83f198a3b755fe0538449952ba3eee8
Ricky Tang <tangricky001@gmail.com>
2025-12-29 23:19:55 -0500

    feat: google analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, PT_Serif } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +44,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-KWE6BQVFHT" />
     </html>
   );
 }
